8071b0ac9ac12e2ab88a3a91890336cdf03018fe
Zac Solomon <zac@zacsolo.com>
2020-08-30 12:17:23 -0400

    Create Todo Route

--- server/database.sql
+++ server/database.sql
@@ -2,5 +2,6 @@ CREATE DATABASE perntodo;
 
 CREATE TABLE todo
 (
-    todo_id SERIAL PRIMARY KEY
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
 );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 //--Middleware--
 app.use(cors());
 app.use(express.json());
+
+//--Routes--
+
+//----Create a todo--
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description]
+    );
+    res.json(newTodo);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+//----Get all todos--
+//----Get a todo--
+//----Update a todo--
+//----Delete a todo--
 
 const PORT = 5000;
 app.listen(PORT, () => {
